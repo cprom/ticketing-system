71f9b514a8fcfd9fee22edb58efc16cdc977a750
Chen Prom <cprom@MacBookPro.lan>
2026-01-30 12:44:57 -0800

    create user list table

--- client/src/pages/Users/Users.jsx
+++ client/src/pages/Users/Users.jsx
@@ -1,31 +1,12 @@
 import React from 'react'
-import { useQuery } from '@tanstack/react-query';
+import UsersList from './UsersList'
 
-const apiUrl = import.meta.env.VITE_BASE_API_URL;
-
-function Users() {
-
-
-    const getUser = async () => {
-        const response = await fetch(`${apiUrl}/api/users`);
-        return await response.json();
-    }
-
-    const { data, error, isPending } = useQuery({
-        queryKey: ['tickets'],
-        queryFn: getUser
-    });
-
-    if(error){
-        console.log(`User Fetching Error: ${error}`);
-    }
-
-    console.log(data)
-  return (
-    <div>
-      Users
-    </div>
-  )
+const Users = () => {
+return (
+<>                          
+    <UsersList/>
+</>
+)
 }
 
 export default Users
---
+++ client/src/pages/Users/UsersList.jsx
@@ -0,0 +1,90 @@
+import {useState} from 'react'
+import { useQuery } from '@tanstack/react-query';
+import { Button, Spin, Tooltip } from "antd";
+import { Table, Badge, Space, Switch } from 'antd';
+import {
+  PlusOutlined,
+} from '@ant-design/icons';
+
+const apiUrl = import.meta.env.VITE_BASE_API_URL;
+
+const UsersList = () => {
+
+      const [show, setShow] = useState(true);
+      const [fetchedData, setFetchData] = useState([]);
+
+    const getUser = async () => {
+        const response = await fetch(`${apiUrl}/api/users`);
+        const results = await response.json();
+        setFetchData(results);
+        return results
+    }
+
+    const { data, error, isPending } = useQuery({
+        queryKey: ['users'],
+        queryFn: getUser
+    });
+
+    if(error){
+        console.log(`User Fetching Error: ${error}`);
+    }
+
+
+    const columns = [
+        {
+            title: 'User ID',
+            dataIndex: 'UserID',
+            defaultSortOrder: 'descend',
+            key: 'UserID',
+            sorter: (a,b) => a.UserID - b.UserID,
+            render: text => <a href={`/users/${text}`} >{text}</a>
+        },
+        {
+            title: 'Full Name',
+            dataIndex: 'FullName',
+            key: 'FullName',
+            sorter: (a,b) => a.FullName - b.FullName,
+            render: text => <a href={`/users/${text}`} >{text}</a>
+        },
+        {
+            title: 'Email',
+            dataIndex: 'Email',
+            key: 'Email',
+            ellipsis: true
+        },
+        {
+            title: 'RoleID',
+            dataIndex: 'RoleID',
+            key: 'RoleID'
+        },
+        {
+            title: 'Action',
+            dataIndex: 'UserID',
+            key: 'UserID',
+            fixed: 'end',
+            width: 100,
+            render: (text) =><a href={`/users/${text}`} >View</a>
+        }
+    ]
+
+    console.log(data)
+  return (
+    <div>
+       <div className='add-new-ticket-btn'>
+            <Button color="default" variant='solid' icon={<PlusOutlined/>} href='/user/new'>New User</Button>
+        </div>
+        { isPending 
+        ? <div className='spinner-container'><Spin /></div> 
+        : <Table
+        columns={columns}
+        dataSource={fetchedData}
+        showSorterTooltip={{target: 'sorter-icon'}}
+        rowKey={record => record.UserID}
+        />
+        }
+    
+    </div>
+  )
+}
+
+export default UsersList
